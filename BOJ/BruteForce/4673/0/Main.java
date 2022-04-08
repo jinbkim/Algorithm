c6baddc62c9b1410b56f15d7f6300a89c1297d35
jinbkim <topmedia3@naver.com>
2022-04-08 13:38:32 +0900

    Update : "BOJ Brute Force"

---
+++ BOJ/BruteForce/4673/0/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+	public static int makeNextSelfNumber(int num) {
+		int nextSelfNumber = num;
+
+		while(num > 0) {
+			int singleDigit = num - num/10*10;
+			nextSelfNumber += singleDigit;
+			num /= 10;
+		}
+		
+		return nextSelfNumber;
+	}
+	
+	public static void main(String[] args) {
+		int maxNumber = 10000;
+		boolean []flag = new boolean[maxNumber];
+		
+		for(int i=1; i<maxNumber; i++)
+		{
+			int nextSelfNumber = makeNextSelfNumber(i);
+			if (nextSelfNumber < maxNumber)
+				flag[nextSelfNumber] = true;
+		}
+		
+		for(int i=1; i<maxNumber; i++)
+			if (flag[i] == false)
+				System.out.println(i);
+	}
+
+}
