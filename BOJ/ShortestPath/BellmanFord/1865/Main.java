6d02edd3479c821dfc2ba6bb6af8ba7bc1c56e59
jinbkim <topmedia3@naver.com>
2022-04-14 13:57:17 +0900

    Add : "BOJ Bellman Ford"

--- BOJ/ShortestPath/BellmanFord/1865/Main.java
+++ BOJ/ShortestPath/BellmanFord/1865/Main.java
@@ -19,36 +19,28 @@ class Edge {
 }
 
 public class Main {
-
     static int n, m, w;
-    static List<Edge> edges = new ArrayList<>();
-    static long[] shortestPath;
+    static int[] shortestPath;
     static int INF = 987654321;
-    // static boolean visit[];
+    static List<Edge> list = new ArrayList<>();
 
-    static boolean bellmanFord() {
+    static boolean isCycle() {
         shortestPath[1] = 0;
 
         for(int i=0; i<=n; i++) {
-            for(int j=0; j<edges.size(); j++) {
-                Edge edge = edges.get(j);
+            for(int j=0; j<list.size(); j++) {
+                Edge edge = list.get(j);
                 if (shortestPath[edge.destination] <= shortestPath[edge.source]+edge.weight)
                     continue;
                 shortestPath[edge.destination] = shortestPath[edge.source]+edge.weight;
-
+                
                 if (i == n)
-                    return false;
+                    return true; 
             }
         }
-        
-        // for(int i=1; i<=n; i++) {
-        //     if (shortestPath[i] == INF)
-        //         return false;
-        // }
 
-        return true;
+        return false;
     }
-
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
@@ -61,52 +53,33 @@ public class Main {
             m = Integer.parseInt(st.nextToken());
             w = Integer.parseInt(st.nextToken());
 
-            edges.clear();
             int source, destination, weight;
-            // visit = new boolean[n+1];
+            list.clear();
             for(int j=0; j<m; j++) {
                 st = new StringTokenizer(br.readLine());
                 source = Integer.parseInt(st.nextToken());
                 destination = Integer.parseInt(st.nextToken());
                 weight = Integer.parseInt(st.nextToken());
-                edges.add(new Edge(source, destination, weight));
-                edges.add(new Edge(destination, source, weight));
-                // visit[source] = true;
+                list.add(new Edge(source, destination, weight));
+                list.add(new Edge(destination, source, weight));
             }
-            
+
             for(int j=0; j<w; j++) {
                 st = new StringTokenizer(br.readLine());
                 source = Integer.parseInt(st.nextToken());
                 destination = Integer.parseInt(st.nextToken());
                 weight = Integer.parseInt(st.nextToken());
-                edges.add(new Edge(source, destination, -weight));
-                // visit[source] = true;
+                list.add(new Edge(source, destination, -weight));
             }
-            
-            // int k=1;
-            // for(int j=1; j<=n; j++) {
-            //     if (visit[j] == false) {
-            //         sb.append("NO").append('\n');
-            //         k = n+1;
-            //         break ;
-            //     }
-            // }
 
-            shortestPath = new long[n+1];
+            shortestPath = new int[n+1];
             Arrays.fill(shortestPath, INF);
-            if (bellmanFord() == false)
-                sb.append("YES").append('\n');
+            if (isCycle() == true)
+                sb.append("YES\n");
             else
-                sb.append("NO").append('\n');
-            // for(; k<=n; k++) {
-            //     if (bellmanFord(k) == false) {
-            //         sb.append("YES").append('\n');
-            //         break ;
-            //     }
-            // }
-            // if (n < k)
-            //     sb.append("NO").append('\n');
+                sb.append("NO\n");
         }
+
         System.out.print(sb.toString());
     }
 }
